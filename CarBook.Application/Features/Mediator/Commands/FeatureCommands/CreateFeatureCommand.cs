bd16581df3768236558976940707c5c9ce564fc5
Emirhan Kılıçaslan <emirhannkilicaslann@gmail.com>
2024-09-19 13:15:11 +0300

    Entities added with Mediator pattern.

---
+++ CarBook.Application/Features/Mediator/Commands/FeatureCommands/CreateFeatureCommand.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+
+public class CreateFeatureCommand : IRequest
+{
+    public string Name { get; set; }
+}
