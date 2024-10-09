7e23232938a79f9358f84a2971e77f611810dc43
Emirhan Kılıçaslan <emirhannkilicaslann@gmail.com>
2024-10-09 12:45:54 +0300

    UI Layout components added.

---
+++ CarBook.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers;
+
+public class AboutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ CarBook.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers;
+
+public class UILayoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
